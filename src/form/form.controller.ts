518be6f3b14109e0a2d5ac6527f29b78ede80cbc
dejita <108794868+nftdejita@users.noreply.github.com>
2024-06-21 07:04:53 +0000

    ひな形修正

--- src/form/form.controller.ts
+++ src/form/form.controller.ts
@@ -1,5 +1,14 @@
 // form/form.controller.ts
-import { Controller, UseGuards, Request, Get, Put, Body, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Request,
+  Get,
+  Put,
+  Body,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FormService } from './form.service';
 
@@ -7,26 +16,27 @@ import { FormService } from './form.service';
 export class FormController {
   constructor(private readonly formService: FormService) {}
 
-  @Get('profile')
+  @Get()
   @UseGuards(AuthGuard('jwt'))
   async getProfile(@Request() req) {
-    console.log(req.user);
+    console.log('GET', req.user);
     const username = req.user?.username;
-    //return { username: username };
     return this.formService.getProfile(username);
   }
 
   @Put()
   @UseGuards(AuthGuard('jwt'))
   async updateProfile(@Request() req, @Body() formData) {
-    console.log(req.user);
+    console.log('Put', req.user);
     const username = req.user?.username;
     return this.formService.updateProfile(username, formData);
   }
 
-  @Delete(':username')
+  @Delete()
   @UseGuards(AuthGuard('jwt'))
-  async deleteProfile(@Param('username') username: string) {
+  async deleteProfile(@Request() req) {
+    console.log('Delete', req.user);
+    const username = req.user?.username;
     return this.formService.deleteProfile(username);
   }
 }
--- src/form/form.service.ts
+++ src/form/form.service.ts
@@ -14,26 +14,45 @@ export class FormService {
 
   async getProfile(username: string): Promise<any> {
     const user = await this.userRepository.findOne({ where: { username } });
+    let profileData;
+
     if (user) {
-      return JSON.parse(user.value); // JSONデータをパースして返す
+      profileData = JSON.parse(user.value); // JSONデータをパースして取得
     } else {
-      return formJson;
+      profileData = { ...formJson }; // form.jsonのコピーを作成
     }
+
+    // usernameフィールドを追加
+    profileData.username = username;
+
+    return profileData;
   }
 
   async updateProfile(username: string, formData: any): Promise<any> {
     // formJsonのコピーを作成し、必要なフィールドのみを更新
-    let updatedFields = formJson.fields.map(field => {
+    let updatedFields = formJson.fields.map((field) => {
       if (formData[field.id] !== undefined) {
         return { ...field, value: formData[field.id] };
       }
       return field;
     });
 
-    // メッセージを更新
+    // 【算術例】ageフィールドの値を使ってbirthYearフィールドを更新する
+    if (formData.age) {
+      updatedFields = updatedFields.map((field) => {
+        if (field.id === 'birthYear') {
+          return { ...field, value: new Date().getFullYear() - formData.age };
+        }
+        return field;
+      });
+    }
+
+    // ヘッダー部分を追加する
     let updatedData = {
+      title: formJson.title,
+      description: formJson.description,
+      username: username,
       fields: updatedFields,
-      message: "データが正常に更新されました。"
     };
 
     // ユーザーエンティティを検索または新規作成
@@ -46,16 +65,16 @@ export class FormService {
     user.value = JSON.stringify(updatedData);
 
     await this.userRepository.save(user);
-    return { message: 'Profile updated successfully' };
+    return { message: 'データが正常に更新されました。' };
   }
 
   async deleteProfile(username: string): Promise<any> {
     const user = await this.userRepository.findOne({ where: { username } });
     if (user) {
       await this.userRepository.remove(user);
-      return { message: 'Profile deleted successfully' };
+      return { message: 'データが正常に削除されました。' };
     } else {
-      return { message: 'User not found' };
+      return { message: '削除対象がありません' };
     }
   }
 }
